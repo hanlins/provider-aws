f2e9dec0b2ed09d287d8b65cfc2394bf0650a37d
Darryl Sw <darryl_sw@hive.gov.sg>
2021-08-27 15:08:21 +0800

    Add postCreate, postObserve, and filterList for VPCEndpoint
    Signed-off-by: Darryl Sw <darryl_sw@hive.gov.sg>

--- pkg/controller/ec2/vpcendpoint/setup.go
+++ pkg/controller/ec2/vpcendpoint/setup.go
@@ -10,12 +10,16 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
 	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 
+	"github.com/aws/aws-sdk-go/aws"
 	svcsdk "github.com/aws/aws-sdk-go/service/ec2"
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	svcapitypes "github.com/crossplane/provider-aws/apis/ec2/v1alpha1"
+	awsclients "github.com/crossplane/provider-aws/pkg/clients"
 )
 
 // SetupVPCEndpoint adds a controller that reconciles VPCEndpoint.
@@ -24,6 +28,10 @@ func SetupVPCEndpoint(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimit
 	opts := []option{
 		func(e *external) {
 			e.preCreate = preCreate
+			e.postCreate = postCreate
+			e.postObserve = postObserve
+			e.isUpToDate = isUpToDate
+			e.filterList = filterList
 		},
 	}
 	return ctrl.NewControllerManagedBy(mgr).
@@ -44,7 +52,7 @@ func preCreate(_ context.Context, cr *svcapitypes.VPCEndpoint, obj *svcsdk.Creat
 	// set external name as tag on the vpc endpoint
 	resType := "vpc-endpoint"
 	key := "Name"
-	value := cr.ObjectMeta.Name
+	value := meta.GetExternalName(cr)
 
 	// Clear SGs, RTs, and Subnets if they're empty
 	if cr.Spec.ForProvider.SecurityGroupIDs == nil || len(cr.Spec.ForProvider.SecurityGroupIDs) == 0 {
@@ -67,7 +75,53 @@ func preCreate(_ context.Context, cr *svcapitypes.VPCEndpoint, obj *svcsdk.Creat
 			},
 		},
 	}
-	obj.TagSpecifications = append(obj.TagSpecifications, &spec)
 
+	obj.TagSpecifications = append(obj.TagSpecifications, &spec)
 	return nil
+}
+
+func postCreate(ctx context.Context, cr *svcapitypes.VPCEndpoint, obj *svcsdk.CreateVpcEndpointOutput, cre managed.ExternalCreation, err error) (managed.ExternalCreation, error) {
+	if err != nil || obj.VpcEndpoint == nil {
+		return managed.ExternalCreation{}, err
+	}
+	// set peering connection id as external name annotation on k8s object after creation
+
+	meta.SetExternalName(cr, aws.StringValue(obj.VpcEndpoint.VpcEndpointId))
+	cre.ExternalNameAssigned = true
+	return cre, nil
+}
+
+func postObserve(_ context.Context, cr *svcapitypes.VPCEndpoint, resp *svcsdk.DescribeVpcEndpointsOutput, obs managed.ExternalObservation, err error) (managed.ExternalObservation, error) {
+	if err != nil {
+		return managed.ExternalObservation{}, err
+	}
+
+	switch awsclients.StringValue(resp.VpcEndpoints[0].State) {
+	case "available":
+		cr.SetConditions(xpv1.Available())
+	case "pending", "pending-acceptance":
+		cr.SetConditions(xpv1.Creating())
+	case "deleted":
+		cr.SetConditions(xpv1.Unavailable())
+	case "deleting":
+		cr.SetConditions(xpv1.Deleting())
+	}
+
+	return obs, nil
+}
+
+func isUpToDate(*svcapitypes.VPCEndpoint, *svcsdk.DescribeVpcEndpointsOutput) (bool, error) {
+	return true, nil
+}
+
+func filterList(cr *svcapitypes.VPCEndpoint, obj *svcsdk.DescribeVpcEndpointsOutput) *svcsdk.DescribeVpcEndpointsOutput {
+	connectionIdentifier := aws.String(meta.GetExternalName(cr))
+	resp := &svcsdk.DescribeVpcEndpointsOutput{}
+	for _, vpcEndpoint := range obj.VpcEndpoints {
+		if aws.StringValue(vpcEndpoint.VpcEndpointId) == aws.StringValue(connectionIdentifier) {
+			resp.VpcEndpoints = append(resp.VpcEndpoints, vpcEndpoint)
+			break
+		}
+	}
+	return resp
 }
