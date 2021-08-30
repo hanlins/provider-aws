51b864726633e3efe4b16d2eccb8ed26d42e3ba4
Darryl Sw <darryl_sw@hive.gov.sg>
2021-08-30 17:53:33 +0800

    Add the delete function for VPCEndpoint
    Signed-off-by: Darryl Sw <darryl_sw@hive.gov.sg>

--- pkg/controller/ec2/vpcendpoint/setup.go
+++ pkg/controller/ec2/vpcendpoint/setup.go
@@ -6,6 +6,7 @@ import (
 
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	"github.com/crossplane/crossplane-runtime/pkg/event"
@@ -14,10 +15,13 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/pkg/errors"
 
 	"github.com/aws/aws-sdk-go/aws"
 	svcsdk "github.com/aws/aws-sdk-go/service/ec2"
+	svcsdkapi "github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	cpresource "github.com/crossplane/crossplane-runtime/pkg/resource"
 	svcapitypes "github.com/crossplane/provider-aws/apis/ec2/v1alpha1"
 	awsclients "github.com/crossplane/provider-aws/pkg/clients"
 )
@@ -27,6 +31,8 @@ func SetupVPCEndpoint(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimit
 	name := managed.ControllerName(svcapitypes.VPCEndpointGroupKind)
 	opts := []option{
 		func(e *external) {
+			c := &custom{client: e.client, kube: e.kube}
+			e.delete = c.delete
 			e.preCreate = preCreate
 			e.postCreate = postCreate
 			e.postObserve = postObserve
@@ -46,6 +52,11 @@ func SetupVPCEndpoint(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimit
 			managed.WithPollInterval(poll),
 			managed.WithLogger(l.WithValues("controller", name)),
 			managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
+}
+
+type custom struct {
+	kube   client.Client
+	client svcsdkapi.EC2API
 }
 
 func preCreate(_ context.Context, cr *svcapitypes.VPCEndpoint, obj *svcsdk.CreateVpcEndpointInput) error {
@@ -84,8 +95,8 @@ func postCreate(ctx context.Context, cr *svcapitypes.VPCEndpoint, obj *svcsdk.Cr
 	if err != nil || obj.VpcEndpoint == nil {
 		return managed.ExternalCreation{}, err
 	}
-	// set peering connection id as external name annotation on k8s object after creation
 
+	// set vpc endpoint id as external name annotation on k8s object after creation
 	meta.SetExternalName(cr, aws.StringValue(obj.VpcEndpoint.VpcEndpointId))
 	cre.ExternalNameAssigned = true
 	return cre, nil
@@ -112,6 +123,22 @@ func postObserve(_ context.Context, cr *svcapitypes.VPCEndpoint, resp *svcsdk.De
 
 func isUpToDate(*svcapitypes.VPCEndpoint, *svcsdk.DescribeVpcEndpointsOutput) (bool, error) {
 	return true, nil
+}
+
+func (e *custom) delete(_ context.Context, mg cpresource.Managed) error {
+	cr, ok := mg.(*svcapitypes.VPCEndpoint)
+	if !ok {
+		return errors.New(errUnexpectedObject)
+	}
+
+	// Generate Deletion Input
+	deleteInput := &svcsdk.DeleteVpcEndpointsInput{}
+	externalName := meta.GetExternalName(cr)
+	deleteInput.SetVpcEndpointIds([]*string{&externalName})
+
+	// Delete
+	_, err := e.client.DeleteVpcEndpoints(deleteInput)
+	return err
 }
 
 func filterList(cr *svcapitypes.VPCEndpoint, obj *svcsdk.DescribeVpcEndpointsOutput) *svcsdk.DescribeVpcEndpointsOutput {
